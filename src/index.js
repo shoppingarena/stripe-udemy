77ecf20aef48a8c7fbc8bdc8b513154898d936e5
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-01-09 20:35:22 +0200

    created component ProductsContextProvider and add to index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ProductsContextProvider from './context/products-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-    <App />
+        <ProductsContextProvider>
+            <App />
+        </ProductsContextProvider>
     </BrowserRouter>
     
   
