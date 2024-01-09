82424241483b1e75bba804c0ac9876202a54feb2
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-01-09 20:14:39 +0200

    styling main section + reesponsive @media condition

--- src/components/main-section/main-section.jsx
+++ src/components/main-section/main-section.jsx
@@ -11,15 +11,18 @@ const MainSection = () => {
     }
 
     return (
-        <div className="main-section-container">
+        <div className='main-section-container'>
             <div className="main-section-middle">
                 <div className="ms-m-image">
                     <img src={studioBag} alt='studio bag' />
                 </div>
                 <div className="ms-m-description">
                     <h2>Design for fashion. Crafted for sport.</h2>
-                    <p>We make products that effortlessly transition from day to night...</p>
-                    <button className="buton is-bulma" id='shop-now' onClick={handleClick}>
+                    <p>We make products that effortlessly transition from day to night. 
+            From the board room to the fitness studio, and everywhere in between, 
+            each Nomads piece is thoughtfully created to be the perfect balance of 
+            form and function.</p>
+                    <button className="button is-black" id='shop-now' onClick={handleClick}>
                         STUDIO BAG
                     </button>
                 </div>
