f44a5fd5b9dece90d6d6f45516594910433158e2
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-01-16 20:26:48 +0200

    created shop component + useContext  from ProductsContext

--- src/components/pages/shop/shop.jsx
+++ src/components/pages/shop/shop.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "../../shared/layout";
 import FeaturedProduct from "../../shared/featured-product";
 import { ProductsContext } from "../../../context/products-context";
 import "./shop.styles.scss";
 
 const Shop = () => {
+    const { products } = useContext(ProductsContext);
+    const allProducts = products.map(product => (
+        <FeaturedProduct { ...product} key={product.id} />
+    ));
+
     return (
-        
+        <div className="">
+            <h2>Shop</h2>
+            <div>
+                { allProducts }
+            </div>
+        </div>
     );
 }
