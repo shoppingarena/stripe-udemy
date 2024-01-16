001a1891ccdceb26425b1fd5225a6101644fc5cf
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-01-16 20:17:59 +0200

    created default shop component in pages folder

---
+++ src/components/pages/shop/shop.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../../shared/layout";
+import FeaturedProduct from "../../shared/featured-product";
+import { ProductsContext } from "../../../context/products-context";
+import "./shop.styles.scss";
+
+const Shop = () => {
+    return (
+        
+    );
+}
