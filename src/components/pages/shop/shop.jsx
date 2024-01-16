d9f70c699108c6155352d81ca0ae62b5315e172d
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-01-16 20:14:53 +0200

    created folder pages in components

--- src/components/pages/shop/shop.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layout from "../../shared/layout";
-import FeaturedProduct from "../../shared/featured-product";
-import { ProductsContext } from "../../../context/products-context";
-import "./shop.styles.scss";
-
-const Shop
-
