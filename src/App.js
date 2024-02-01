aa6c81cf2cb390be22f95ab5123e47bed48a9c8e
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-02-01 22:56:48 +0200

    26. Add Product

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProductsContextProvider from './context/products-context';
+import CartContextProvider from './context/cart-context';
 import HomePage from './components/home-page';
 import NotFound from './components/not-found';
 import Shop from './components/pages/shop/shop';
@@ -10,15 +11,17 @@ function App() {
   return (
     <>
     <BrowserRouter>
-    <ProductsContextProvider>
-      <Routes>
-        {/*<div className='App'>*/}
-          <Route path="/" element={<HomePage />} />
-          <Route path='/shop' element={<Shop />} />
-          <Route path='/product/:id' element={<SingleProduct />} />
-          <Route path="*" element={<NotFound />} />
-        {/*</div>*/}
-      </Routes>
+      <ProductsContextProvider>
+        <CartContextProvider>
+          <Routes>
+            {/*<div className='App'>*/}
+              <Route path="/" element={<HomePage />} />
+              <Route path='/shop' element={<Shop />} />
+              <Route path='/product/:id' element={<SingleProduct />} />
+              <Route path="*" element={<NotFound />} />
+            {/*</div>*/}
+          </Routes>
+        </CartContextProvider>
       </ProductsContextProvider>
     </BrowserRouter>
     
--- src/context/cart-context.jsx
+++ src/context/cart-context.jsx
@@ -8,9 +8,10 @@ const initialState = { cartItems: [], itemCount: 0, total: 0};
 
 const CartContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(cartReducer, initialState);
-
+    const addProduct = (product) => dispatch({ type: 'ADD_ITEM', payload: product});
 const contextValues = {
     ...state,
+    addProduct,
 }
 
 return (
