292cfea01ed7aba07948b7dad672d16d73a98ca5
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-02-03 21:39:54 +0200

    Styling added for CartItem

--- src/components/pages/cart-page/cart-items.jsx
+++ src/components/pages/cart-page/cart-items.jsx
@@ -12,8 +12,8 @@ const { title, imageUrl, proce, quantity } = product;
 
 // we going to return some jsx
 return (
-    <div>
-        <div>
+    <div className="cart-item">
+        <div className="item-image">
             <img src={imageUrl} alt='product' />
         </div>
         <div className="name-price">
@@ -24,18 +24,18 @@ return (
             <p>Quantity: ${quantity}</p>
         </div>
         <div className="btns-container">
-            <button>
+            <button className="btn-increase">
                 <PlusCircleIcon width='20px' />
             </button>
             {   // testing to show Trash button if there is some quantity
                 quantity === 1 &&
-                <button>
+                <button className="btn-trash">
                     <TrashIcon width='20px' />
                 </button>
             }
             { // another testing to show  Increase button if there is at least min 1 quantity
                 quantity > 1 &&
-                <button>
+                <button className="btn-decrease">
                     <MinusCircleIcon width='20px' />
                 </button>
             }
