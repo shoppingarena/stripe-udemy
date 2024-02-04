0d1a02c2a1430885d231ee5a16b8a5e5b738b2a1
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-02-04 15:41:48 +0200

    added click to checkout with useNavigate

---
+++ src/components/pages/cart-page/total.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Total = ({ itemCount, total }) => {
+        let navigate = useNavigate();
+        const clickCheckout = () => {
+            navigate('/checkout');
+        };
+
+
+    return (
+        <div className="total-container">
+            <div className="total">
+                <p>Total Items: {itemCount}</p>
+                <p>{`Total: ${total}`}</p>
+            </div>
+            <div className="checkout">
+                <button className="button is-black" onClick={clickCheckout}>CHECKOUT</button>
+            </div>
+
+        </div>
+    )
+}
