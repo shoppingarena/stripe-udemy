c60eef61504058c3a607d3065022eccc74793d31
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-02-16 05:20:05 +0200

    feat: checkout component

--- src/components/checkout/checkout.jsx
+++ src/components/checkout/checkout.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, {useContext } from "react";
 import { CartContext } from '../../context/cart-context';
 import { Layout } from '../shared/layout';
 import './checkout.styles.scss';
 
+const Checkout = () => {
+    const { itemCount, total } = useContext(CartContext);
 
+    return (
+        <Layout>
+        <div>
+            <h2 className='checkout'>Checkout Summary</h2>
+            <h3>{`Total Items: ${itemCount}`}</h3>
+            <h3>{`Àmount to Pay: ${total}`}</h3>
+        </div>
+        </Layout>
+    )
+}
+
+export default Checkout;
