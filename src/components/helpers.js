4f25f40ae0cbd9f5c2ab8de03a0210cb7fd49fa1
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-02-17 20:55:40 +0200

    request server fetchFromAPI

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,3 +1,21 @@
 export const isInCart = (product, cartItems) => {
     return cartItems.find(item => item.id === product.id);
 }
+
+//going to request server
+const API = 'http://localhost:8080';
+
+export async function fetchFromAPI(endpoint, opts) {
+    const { method, body } = { method: 'POST', body: null, ...opts };
+    
+    const res = await fetch(`${API}/${endpoint}`, {
+        method,
+        ...(body && { body: JSON.stringify(body) }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    return res.json();
+
+}    
