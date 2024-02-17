ac44388b4d9cad13eb2f4a69c320173af3ec2274
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-02-17 21:10:30 +0200

    feat: StripeCheckout component

--- src/components/checkout/stripe-checkout/stripe-checkout.jsx
+++ src/components/checkout/stripe-checkout/stripe-checkout.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useStripe } from '@stripe/react-stripe-js';
 import { CartContext } from "../../../context/cart-context";
 import { fetchFromAPI } from "../../helpers";
 
 const StripeCheckout = () => {
-    
+    const [email, setEmail] = useState('');
+    const { cartItems } = useContext(CartContext);
+
+    //define handler
+    const handleGuestCheckout = () => {}
+
+    return (
+        <form onSubmit={handleGuestCheckout}>
+            <div>
+                <input type='email' onChange={e => setEmail(e.target.value)}
+                    placeholder='Email'
+                    value={email}
+                    className='nomad-input'
+                />
+            </div>
+            <div className="submit-btn">
+            <button type='submit' className="button is-black nomad-btn submit">
+                Checkout
+            </button>
+
+            </div>
+        </form>
+    );
 }
+
+export default StripeCheckout;
