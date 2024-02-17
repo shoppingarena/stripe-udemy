5dca4c78ec65336a09c7fe34973187bef137f14b
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-02-17 21:00:10 +0200

    feat: StripeCheckout component

---
+++ src/components/checkout/stripe-checkout/stripe-checkout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useStripe } from '@stripe/react-stripe-js';
+import { CartContext } from "../../../context/cart-context";
+import { fetchFromAPI } from "../../helpers";
+
+const StripeCheckout = () => {
+    
+}
