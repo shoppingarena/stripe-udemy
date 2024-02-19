48016dbdf012491abdc9c042e92df41ea449a85d
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-02-19 21:46:51 +0200

    feat: Route Success

--- src/components/checkout/success.jsx
+++ src/components/checkout/success.jsx
@@ -5,13 +5,21 @@ import { useNavigate } from "react-router-dom";
 
 const Success = () => {
     const { clearCart} = useContext(CartContext);
-    useEffect(clearCart, []);
+    /**
+     * code before: useEffect(clearCart, []);
+     * React Hook useEffect has a missing dependency: 'clearCart'.
+     * Either include it or remove the dependency array  react-hooks/exhaustive-deps
+     */
+    useEffect(() => {
+        clearCart();
+    }, [clearCart]);
+
     let navigate = useNavigate();
     const clickContinue = () => {navigate('/shop');}
 
     return (
         <Layout>
-        <div>
+        <div className="checkout">
             <h1>Thank you for your order</h1>
             <p>WE are currently processing your order and will send you a confirmation email shortly.</p>
             <div>
