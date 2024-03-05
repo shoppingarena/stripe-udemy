a0953eca947ba18fadf184c3764ddff0cb8a9caa
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-03-05 12:01:40 +0200

    feat: shipping address component + input name

---
+++ src/components/checkout/custom-checkout/shipping-address.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Formik } from  "formik";
+
+const ShippingAddress = () => {
+    return (
+        <div>
+            <h4>Shipping Address</h4>
+            <Formik>
+                {
+                    ({ values, errors, handleChange, handleSubmit }) => {
+                        const { name, email, address } = errors;
+                        return (
+                            <form onSubmit={handleSubmit}>
+                                <div>
+                                    <input 
+                                        type='text'
+                                        name='name'
+                                        onChange={handleChange}
+                                        value={values.name}
+                                        className={ 'nomad-input' + (name? 'error' : '')}
+                                    />
+                                </div>
+                            </form>
+                        )
+                    }
+                }
+            </Formik>
+        </div>
+    )
+}
